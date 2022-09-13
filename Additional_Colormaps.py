eb623324984710220d0713916758ee2048d26f3d
bgeyman <56602673+bgeyman@users.noreply.github.com>
2022-09-13 16:40:52 -0400

    update function names, add new cmap

--- Additional_Colormaps.py
+++ Additional_Colormaps.py
@@ -4,7 +4,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-def make_colormap(path='./colormap_files/', filename='WhiteBlueGreenYellowRed.rgb', output_name='test'):
+path = '/Users/bengeyman/Documents/Github/Additional_Colormaps/colormap_files/'
+
+def make_colormap(path=path, filename='WhiteBlueGreenYellowRed.rgb', output_name='test'):
 
     data = pd.read_csv(path+filename, sep="/", header=1)
 
@@ -21,7 +23,7 @@ def make_colormap(path='./colormap_files/', filename='WhiteBlueGreenYellowRed.rg
     newcmp = ListedColormap(new_data, name=output_name)
     return newcmp
 
-def load_all_NCL_cmaps():
+def load_all_cmaps():
     ## import cmap from NCL gallery
     ## many more here: https://www.ncl.ucar.edu/Document/Graphics/color_table_gallery.shtml
     
@@ -38,17 +40,18 @@ def load_all_NCL_cmaps():
     USGS_15lev_a    = make_colormap(filename='USGS_rainbow_15lev_handmod.rgb', output_name='USGS_rainbow_15_a')
     USGS_15lev_b    = make_colormap(filename='USGS_rainbow_15lev_handmod_b.rgb', output_name='USGS_rainbow_15_b')
     USGS_16lev      = make_colormap(filename='USGS_rainbow_16lev_handmod.rgb', output_name='USGS_rainbow_16')
+    rafaj_10lev     = make_colormap(filename='rafaj_2021_air_quality.rgb', output_name='rafaj_AQ.rgb') # from Peter Rafaj et al 2021 Environ. Res. Lett. 16 045005
     
     #plot_examples(new_cmap)
     maps = {'ndvi': modis_ndvi, 'WBGYR': WBGYR, 'BGYORV':BGYORV, 'precip3_16lev':precip3_16lev,  
             'perc2_9lev': perc2_9lev, 'wind_17lev':wind_17lev, 'cmocean_curl':cmocean_curl,
             'cmocean_ice':cmocean_ice, 'cmocean_thermal':cmocean_thermal, 'USGS_rainbow':USGS, 
             'USGS_rainbow_15lev_a':USGS_15lev_a, 'USGS_rainbow_15lev_b':USGS_15lev_b,
-            'USGS_rainbow_16lev':USGS_16lev}
+            'USGS_rainbow_16lev':USGS_16lev, 'rafaj_AQ':rafaj_10lev}
     
     return maps
 
-def plot_NCL_cmap_example(cmap):
+def plot_cmap_example(cmap):
     """
     helper function to plot colormap swatch
     """
